abedfba6e832ad048dd2642c76d24fac47e44708
Emmanuel Oriarewo <hollaemor@gmail.com>
2020-05-02 23:52:17 +0200

    Setup config service

---
+++ src/main/java/org/hollaemor/movierentals/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package org.hollaemor.movierentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
